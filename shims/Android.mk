aaa67ec6c11e98ec4e0393547b95ae855aaa9091
Tom Marshall <tdm.code@gmail.com>
2017-10-12 13:03:33 -0700

    tulip: Initial device tree

---
+++ shims/Android.mk
@@ -0,0 +1,35 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+  camera_shim.cpp
+
+LOCAL_SHARED_LIBRARIES := \
+  libui
+
+LOCAL_MODULE := libshim_camera
+LOCAL_MODULE_TAGS := optional
+LOCAL_MULTILIB := 32
+
+include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+  power_shim.cpp
+
+LOCAL_MODULE := libshim_power
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+  ril_shim.cpp
+
+LOCAL_MODULE := libshim_ril
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_SHARED_LIBRARY)
